46d86cb9ccddda69e8f66ab817a60a2792696237
Daniel Clarke <u8sand@gmail.com>
2023-03-02 12:07:24 -0500

    Add tooltips to guide user with interface

--- src/ReactSupervenn.tsx
+++ src/ReactSupervenn.tsx
@@ -71,6 +71,7 @@ const ReactSupervenn: React.FC<{
   color_cycle,
   alternating_background,
 }) => {
+  const tooltipRef = React.useRef<HTMLDivElement>(null)
   const [selection, setSelection] = React.useState({})
   const { selectedRows, selectedCols, selectedItems } = React.useMemo(() => {
     const selectedRows = {}
@@ -87,201 +88,230 @@ const ReactSupervenn: React.FC<{
     }
     return { selectedRows, selectedCols, selectedItems }
   }, [selection])
+  React.useEffect(() => {
+    if (!tooltipRef) return
+    const listener = (evt: MouseEvent) => {
+      if (!tooltipRef.current) return
+      const el = document.elementFromPoint(evt.clientX, evt.clientY)
+      const tip = el.getAttribute('data-tip')
+      if (tip !== null) {
+        tooltipRef.current.innerText = tip
+        tooltipRef.current.style.left = `${evt.clientX + window.scrollX}px`
+        tooltipRef.current.style.top = `${evt.clientY + window.scrollY}px`
+        tooltipRef.current.style.opacity = `1.0`
+      } else {
+        tooltipRef.current.style.opacity = `0.0`
+      }
+    }
+    document.addEventListener('mousemove', listener)
+    return () => {
+      document.removeEventListener('mousemove', listener)
+    }
+  }, [tooltipRef])
   return (
-    <div className="react-supervenn-layout">
-      <div className="react-supervenn-data">
-        {composition_array.map((cells, row) => (
-          <div
-            key={row}
-            className={classNames({ 'react-supervenn-alternate': alternating_background && row % 2 == 0 })}
-          >
-            {cells.map((cell, col) => {
-              if (cell === 1) {
-                return (
-                  <div
-                    key={col}
-                    className={classNames({
-                      'react-supervenn-cell': true,
-                      'react-supervenn-selected': selection[`${row}.${col}`],
-                    })}
-                    onClick={_ => {
-                      setSelection(
-                        selection => ({
-                          ...selection,
-                          [`${row}.${col}`]: !selection[`${row}.${col}`],
-                        })
-                      )
-                    }}
-                    style={{
-                      width: as_percent(col_widths[col] / n_items),
-                      backgroundColor: color_by === 'column' ? color_cycle[col % color_cycle.length] : color_cycle[row % color_cycle.length],
-                      userSelect: 'none',
-                    }}>&nbsp;</div>
-                )
-              } else {
-                return (
-                  <div
-                    key={col}
-                    className="react-supervenn-cell"
-                    style={{
-                      width: as_percent(col_widths[col] / n_items),
-                      userSelect: 'none',
-                    }}>&nbsp;</div>
-                )
-              }
-            })}
+    <>
+      <div className="react-supervenn-layout">
+        <div className="react-supervenn-data">
+          {composition_array.map((cells, row) => (
+            <div
+              key={row}
+              className={classNames({ 'react-supervenn-alternate': alternating_background && row % 2 == 0 })}
+            >
+              {cells.map((cell, col) => {
+                if (cell === 1) {
+                  return (
+                    <div
+                      key={col}
+                      className={classNames({
+                        'react-supervenn-cell': true,
+                        'react-supervenn-selected': selection[`${row}.${col}`],
+                      })}
+                      data-tip={`This region has ${cell} item(s) from ${set_annotations[row]}, click to select.`}
+                      onClick={_ => {
+                        setSelection(
+                          selection => ({
+                            ...selection,
+                            [`${row}.${col}`]: !selection[`${row}.${col}`],
+                          })
+                        )
+                      }}
+                      style={{
+                        width: as_percent(col_widths[col] / n_items),
+                        backgroundColor: color_by === 'column' ? color_cycle[col % color_cycle.length] : color_cycle[row % color_cycle.length],
+                        userSelect: 'none',
+                      }}>&nbsp;</div>
+                  )
+                } else {
+                  return (
+                    <div
+                      key={col}
+                      className="react-supervenn-cell"
+                      style={{
+                        width: as_percent(col_widths[col] / n_items),
+                        userSelect: 'none',
+                      }}>&nbsp;</div>
+                  )
+                }
+              })}
+            </div>
+          ))}
+        </div>
+        <div className="react-supervenn-ylabel">
+          <div>SETS (<span>{sets.length} sets</span>)
           </div>
-        ))}
-      </div>
-      <div className="react-supervenn-ylabel">
-        <div>SETS (<span>{Object.keys(selectedRows).length} sets</span>)
+        </div>
+        <div className="react-supervenn-xlabel">
+          ITEMS (<span>{n_items} items</span>)
+        </div>
+        <div className="react-supervenn-yticks">
+          {sets.map((_, row) => (
+            <div
+              key={row}
+              className={classNames({
+                'react-supervenn-selected': selectedRows[row],
+                'react-supervenn-alternate': alternating_background && row % 2 == 0,
+              })}
+              data-tip={`This row has ${sets[row].length} item(s) from ${set_annotations[row]}, click to select.`}
+              onClick={_ => {
+                setSelection(
+                  selection => {
+                    const _selection = {...selection}
+                    for (const col in Object.keys(chunks)) {
+                      if (composition_array[row][col]) {
+                        _selection[`${row}.${col}`] = !selectedRows[row]
+                      }
+                    }
+                    return _selection
+                  }
+                )
+              }}
+            ><span>{set_annotations[row]}</span></div>
+          ))}
+        </div>
+        <div className="react-supervenn-xticks">
+          {chunks.map((chunk, col) => (
+            <div
+              key={col}
+              className={classNames({
+                'react-supervenn-selected': selectedCols[col],
+                'react-supervenn-rotated': rotate_col_annotations,
+              })}
+              style={{
+                width: as_percent(col_widths[col] / n_items),
+              }}
+              data-tip={`This column has ${chunk.length} item(s) shared by ${ycounts[col]} set(s), click to select.`}
+              onClick={_ => {
+                setSelection(
+                  selection => {
+                    const _selection = {...selection}
+                    for (const row in composition_array) {
+                      if (composition_array[row][col]) {
+                        _selection[`${row}.${col}`] = !selectedCols[col]
+                      }
+                    }
+                    return _selection
+                  }
+                )
+              }}
+            >
+              <span>
+                {chunk.length >= effective_min_width_for_annotation ?
+                  chunk.length
+                  : null}
+              </span>
+            </div>
+          ))}
+        </div>
+        <div className="react-supervenn-ycount">
+          {chunks.map((chunk, col) => (
+            <div
+              key={col}
+              style={{
+                width: as_percent(col_widths[col] / n_items),
+              }}
+              data-tip={`This column has ${chunk.length} item(s) shared by ${ycounts[col]} set(s), click to select.`}
+              onClick={_ => {
+                setSelection(
+                  selection => {
+                    const _selection = {...selection}
+                    for (const row in composition_array) {
+                      if (composition_array[row][col]) {
+                        _selection[`${row}.${col}`] = !selectedCols[col]
+                      }
+                    }
+                    return _selection
+                  }
+                )
+              }}
+            >
+              <div
+                style={{
+                  pointerEvents: 'none',
+                  height: as_percent(ycounts[col] / sets.length),
+                }}>
+                  <span>
+                    {chunks[col].length >= effective_min_width_for_annotation ?
+                      ycounts[col]
+                      : null}
+                  </span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="react-supervenn-xcount">
+          {sets.map((_, row) => (
+            <div
+              key={row}
+              className={classNames({ 'react-supervenn-alternate': alternating_background && row % 2 == 0 })}
+              data-tip={`This row has ${sets[row].length} item(s) from ${set_annotations[row]}, click to select.`}
+              onClick={_ => {
+                setSelection(
+                  selection => {
+                    const _selection = {...selection}
+                    for (const col in Object.keys(chunks)) {
+                      if (composition_array[row][col]) {
+                        _selection[`${row}.${col}`] = !selectedRows[row]
+                      }
+                    }
+                    return _selection
+                  }
+                )
+              }}
+            >
+              <div
+                style={{
+                  pointerEvents: 'none',
+                  width: as_percent(sets[row].length / n_items),
+                }}
+              ><span>{sets[row].length}</span></div>
+            </div>
+          ))}
+        </div>
+        <div className="react-supervenn-sets" onClick={evt => {
+          (evt.currentTarget.children[0] as HTMLTextAreaElement).select()
+          navigator.clipboard.writeText(Object.keys(selectedRows).map(s => set_annotations[s]).join('\n'))
+        }}>
+          <textarea
+            rows={3}
+            readOnly
+            value={Object.keys(selectedRows).map(s => set_annotations[s]).join('\n')}
+          />
+          <label>{Object.keys(selectedRows).length} set(s), click to copy</label>
+        </div>
+        <div className="react-supervenn-items" onClick={evt => {
+          (evt.currentTarget.children[0] as HTMLTextAreaElement).select()
+          navigator.clipboard.writeText(Object.keys(selectedItems).join('\n'))
+        }}>
+          <textarea
+            rows={4}
+            readOnly
+            value={Object.keys(selectedItems).join('\n')}
+          />
+          <label>{Object.keys(selectedItems).length} item(s), click to copy</label>
         </div>
       </div>
-      <div className="react-supervenn-xlabel">
-        ITEMS (<span>{Object.keys(selectedItems).length} items</span>)
-      </div>
-      <div className="react-supervenn-yticks">
-        {sets.map((_, row) => (
-          <div
-            key={row}
-            className={classNames({
-              'react-supervenn-selected': selectedRows[row],
-            })}
-            onClick={_ => {
-              setSelection(
-                selection => {
-                  const _selection = {...selection}
-                  for (const col in Object.keys(chunks)) {
-                    if (composition_array[row][col]) {
-                      _selection[`${row}.${col}`] = !selectedRows[row]
-                    }
-                  }
-                  return _selection
-                }
-              )
-            }}
-          ><span>{set_annotations[row]}</span></div>
-        ))}
-      </div>
-      <div className="react-supervenn-xticks">
-        {chunks.map((chunk, col) => (
-          <div
-            key={col}
-            className={classNames({
-              'react-supervenn-selected': selectedCols[col],
-              'react-supervenn-rotated': rotate_col_annotations,
-            })}
-            style={{
-              width: as_percent(col_widths[col] / n_items),
-            }}
-            onClick={_ => {
-              setSelection(
-                selection => {
-                  const _selection = {...selection}
-                  for (const row in composition_array) {
-                    if (composition_array[row][col]) {
-                      _selection[`${row}.${col}`] = !selectedCols[col]
-                    }
-                  }
-                  return _selection
-                }
-              )
-            }}
-          >
-            <span>
-              {chunks[col].length >= effective_min_width_for_annotation ?
-                chunks[col].length
-                : null}
-            </span>
-          </div>
-        ))}
-      </div>
-      <div className="react-supervenn-ycount">
-        {chunks.map((chunk, col) => (
-          <div
-            key={col}
-            style={{
-              width: as_percent(col_widths[col] / n_items),
-            }}
-            onClick={_ => {
-              setSelection(
-                selection => {
-                  const _selection = {...selection}
-                  for (const row in composition_array) {
-                    if (composition_array[row][col]) {
-                      _selection[`${row}.${col}`] = !selectedCols[col]
-                    }
-                  }
-                  return _selection
-                }
-              )
-            }}
-          >
-            <div
-              style={{
-                height: as_percent(ycounts[col] / sets.length),
-              }}>
-                <span>
-                  {chunks[col].length >= effective_min_width_for_annotation ?
-                    ycounts[col]
-                    : null}
-                </span>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="react-supervenn-xcount">
-        {sets.map((_, row) => (
-          <div
-            key={row}
-            className={classNames({ 'react-supervenn-alternate': alternating_background && row % 2 == 0 })}
-            onClick={_ => {
-              setSelection(
-                selection => {
-                  const _selection = {...selection}
-                  for (const col in Object.keys(chunks)) {
-                    if (composition_array[row][col]) {
-                      _selection[`${row}.${col}`] = !selectedRows[row]
-                    }
-                  }
-                  return _selection
-                }
-              )
-            }}
-          >
-            <div
-              style={{
-                width: as_percent(sets[row].length / n_items),
-              }}
-            ><span>{sets[row].length}</span></div>
-          </div>
-        ))}
-      </div>
-      <div className="react-supervenn-sets">
-        <textarea
-          rows={5}
-          readOnly
-          onSelect={evt => {
-            evt.currentTarget.select()
-            navigator.clipboard.writeText(Object.keys(selectedRows).map(s => set_annotations[s]).join('\n'))
-          }}
-          placeholder="Selected sets show up here, click to copy"
-          value={Object.keys(selectedRows).map(s => set_annotations[s]).join('\n')}
-        />
-      </div>
-      <div className="react-supervenn-items">
-        <textarea
-          rows={5}
-          readOnly
-          onSelect={evt => {
-            evt.currentTarget.select()
-            navigator.clipboard.writeText(Object.keys(selectedItems).join('\n'))
-          }}
-          placeholder="Selected items show up here, click to copy"
-          value={Object.keys(selectedItems).join('\n')}
-        />
-      </div>
-    </div>
+      <div ref={tooltipRef} className="react-supervenn-tooltip"></div>
+    </>
   )
 }
 
